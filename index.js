db50489b3a2b513e339a5a7cf058d0464021f622
Malik Touqeer <touqeeryameen@gmail.com>
2024-09-01 09:47:23 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+ stdobt_marks = prompt("enter your obt marks");
+ stdTm_marks = prompt("enter your tm marks");
+ res = (stdobt_marks/stdTm_marks) *100;
+ alert("you have got it"+ res + "%");
+ console.log("you have got it " + res + "%");
+ document.write("you have got it" + res + "%");
